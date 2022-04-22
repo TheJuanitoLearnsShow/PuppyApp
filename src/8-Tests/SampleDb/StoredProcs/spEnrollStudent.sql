a9c59ace241e89ef2546b3ef5e8cc3e8adfaffc6
The Juanito Learns Show <jptarqu@hotmail.com>
2022-04-21 20:32:19 -0400

    Date field fix

--- src/8-Tests/SampleDb/StoredProcs/spEnrollStudent.sql
+++ src/8-Tests/SampleDb/StoredProcs/spEnrollStudent.sql
@@ -2,6 +2,8 @@
 	@FirstName varchar(50)
            ,@LastName varchar(50)
            ,@Age int
+           ,@EnrolledOn datetime
+           ,@GradeLevel int
 AS
 
 
@@ -18,3 +20,6 @@ Select SCOPE_IDENTITY() StudentId;
 
 GO
 
+EXEC sys.sp_addextendedproperty @name=N'Lookup Table for GradeLevel', @value=N'Grade Levels' , 
+@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'PROCEDURE',@level1name=N'spEnrollStudent'
+GO
---
+++ src/8-Tests/SampleDb/Tables/Grade Levels.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Grade Levels]
+(
+	[Grade Level] INT NOT NULL PRIMARY KEY,
+	[Grade Name] varchar(100)
+)
+go
+
+
+EXEC sys.sp_addextendedproperty @name=N'Lookup ID', @value=N'Grade Level' , 
+@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Grade Levels'
+GO
+EXEC sys.sp_addextendedproperty @name=N'Lookup Label', @value=N'Grade Name' , 
+@level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Grade Levels'
+GO
