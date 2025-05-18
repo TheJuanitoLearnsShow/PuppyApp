675efe6f0ab1cdcd0b24a9d6655d00fcb59d5459
The Juanito Learns Show <98781316+TheJuanitoLearnsShow@users.noreply.github.com>
2025-05-17 22:49:25 -0400

    Attempt in WinUI3

---
+++ src/7-Presentation/PuppyApp.ReactiveViewModels/CallParameterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive.Linq;
+using Puppy.Types;
+using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+using System.Reactive.Concurrency;
+
+namespace PuppyApp.ReactiveViewModels;
+
+public partial class CallParameterModel : ReactiveObject
+{
+    public IPrimitivePropertyDescriptor PropertyDescriptor { get; }
+
+    public CallParameterModel(IPrimitivePropertyDescriptor propertyDescriptor)
+    {
+        PropertyDescriptor = propertyDescriptor;
+        
+        _errorHelper = this.WhenAnyValue(x => x.EditValue) 
+            .Throttle(TimeSpan.FromMilliseconds(800), RxApp.TaskpoolScheduler)
+            .DistinctUntilChanged()
+            .ToProperty(this, x => ValidateEditValue(x.EditValue));;
+    }
+    
+    [Reactive]
+    private string _editValue = string.Empty;
+    [Reactive]
+    private string _label = string.Empty;
+    
+    [ObservableAsProperty]
+    private string _error = string.Empty;
+    
+    private string ValidateEditValue(string editValue)
+    {
+        var errors = PropertyDescriptor.Validate(editValue);
+        return string.Join(" | ", errors.Select(e => e.Description));
+    }
+}
---
+++ src/7-Presentation/PuppyApp.WinUI3/Templates/StringToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PuppyApp.WinUI3.Templates;
+
+public class StringToVisibilityConverter : IValueConverter
+{
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
